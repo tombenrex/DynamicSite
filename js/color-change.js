0f9a338b247aa827c1bb321cc91941bbf6350b28
Tom Larsson <tmlsn@hotmail.com>
2025-02-25 06:55:04 +0100

    hidden page

--- js/color-change.js
+++ js/color-change.js
@@ -1,7 +1,8 @@
 function myFunction() {
   var color = document.getElementById("color-input").value;
-  if (color === "background (0, 0, 0)") {
-    alert("NOT BLACK");
+  if (color.toLowerCase() === "#000000") {
+    alert("I told you not so! U");
+    location.href = "hiddenpage.html"; // Copy this line from stackoverflow
   } else {
     document.body.style.background = color;
   }
